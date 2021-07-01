de7292779bf10c22de0c4305c9fc32c03036f643
LENAERTS Tom <t.lenaerts@laposte.net>
2021-07-01 19:10:37 +0200

    Parallax

--- RattrapJam_Project/Assets/Scripts/Parallax.cs
+++ RattrapJam_Project/Assets/Scripts/Parallax.cs
@@ -7,6 +7,8 @@ public class Parallax : MonoBehaviour
     #region Variables
     private Transform cameraTransform;
     private Vector3 lastCameraPosition;
+    private float textureUnitSizeX;
+
     [SerializeField] private float parallaxEffectMultiplier;
     #endregion
 
@@ -14,6 +16,9 @@ public class Parallax : MonoBehaviour
     {
         cameraTransform = Camera.main.transform;
         lastCameraPosition = transform.position;
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
     }
 
     private void LateUpdate()
@@ -21,5 +26,11 @@ public class Parallax : MonoBehaviour
         Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
         transform.position += deltaMovement * parallaxEffectMultiplier;
         lastCameraPosition = cameraTransform.position;
+
+        /*if (Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX)
+        {
+            float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+            transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y);
+        }*/
     }
 }
