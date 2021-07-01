a14be6572f2b5b7dc111d4e078a1b69763ece899
Leo <Aqu4men@gmail.com>
2021-07-01 20:50:21 +0200

    LDfini

--- RattrapJam_Project/Assets/Scripts/GameOn.cs
+++ RattrapJam_Project/Assets/Scripts/GameOn.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameOn : MonoBehaviour
 {
+    public SpriteRenderer nuage;
+    public SpriteRenderer Hills;
     // Start is called before the first frame update
     private GameObject Tornade;
     void Start()
@@ -14,9 +16,12 @@ public class GameOn : MonoBehaviour
     public void Activation()
     {
         GetComponentInParent<Movements>().enabled = true;
+        
     }
     public void TornadeActivation()
     {
+        nuage.color = new Color(0.3f, 0.3f, 0.3f, 1);
+        Hills.color = new Color(0.6f, 0.6f, 0.6f, 1);
         Tornade.GetComponent<TornadoMovement>().enabled = true;
     }
 }
